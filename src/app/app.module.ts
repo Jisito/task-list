cc3da2e3a17b5cb7401b10126df9e77d55d9b362
Jisito <suarezjoaquin4481@gmail.com>
2023-01-26 22:06:58 -0300

    Servicio Y servidor de tareas Listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -20,7 +20,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from '../../components/mock-task';
-import { Task } from '../../components/Task';
+import {  TaskService} from "../../service/task.service";
+import { Task } from '../Task';
+// import { TASKS } from '../../components/mock-task';
+//Cambiamos para usar el service.ts import { Task } from '../../components/Task';
 
 
 @Component({
@@ -9,8 +11,16 @@ import { Task } from '../../components/Task';
   styleUrls: ['./task.component.css'],
 })
 export class TaskComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
   
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(
+    private taskService:TaskService
+    ){}
+  ngOnInit(): void {
+    //like promise
+     this.taskService.getTasks().subscribe((tasks) => (
+      this.tasks = tasks
+    ));
+   
+  }
 }
